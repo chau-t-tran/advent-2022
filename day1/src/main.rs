30860dba0b6d71fcc27e7f54d87f55288c2d8ad8
chau-t-tran <chau.t.tran.us@gmail.com>
2022-12-06 23:28:10 -0500

    Day 1 solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+
 fn main() {
-    println!("Hello, this is day 1!");
+    let file = File::open("day1/input.txt").expect("Error opening file");
+    let reader = BufReader::new(file);
+
+    let mut max = 0;
+    let mut local_max = 0;
+
+    for line_result in reader.lines() {
+        let line = line_result.expect("Error reading line");
+        if line.is_empty() {
+            if local_max > max {
+                max = local_max;
+            }
+            local_max = 0;
+            continue;
+        }
+        local_max += line.parse::<i32>().expect("Error parsing int")
+    }
+
+    println!("Most calories: {}", max);
 }
